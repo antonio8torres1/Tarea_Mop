149f70989550101ab36149daa37a39eb5aa0c2d4
Antonio <antonio8torres1@gmail.com>
2025-05-18 00:00:44 -0600

    Agregar clase y funciones principales para trabajar con metodo de la M

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 from numpy import array
-from tablesimplex import TableSimplex
+from simplex_m import Simplex
 
 def main():
     
-    z = array([-1.6,-1.4,0,0,0],dtype=float)
-    matrix = array([
-        [10,20,1,0,0],
-        [15,10,0,1,0],
-        [18,6,0,0,1],
-    ],dtype=float)
+    matrix = [
+        [5, 6, 0, 0, 0, 0,0],
+        [2, 3, -1, 0, 1, 0,12],
+        [2, 1, 0, -1, 0, 1,14]
+    ]
+    header = ["X1","X2","H1","H2","A1","A2"]
+    ld = ["Z","A1","A2"]
 
-    ld = array([0,8000,6000,6300],dtype=float)
-    header = ["X1","X2","H1","H2","H3"]
-    vb = ["Z ","H1","H2","H3"]
-
-    table = TableSimplex(z,matrix,ld,header,vb,1)
-
+    table = Simplex(3,7)
+    table.initialize(matrix,header,ld)
     print(table)
-
-    n = table.solve()
-
-    print(table)
-    print(n)
 
 if __name__ == "__main__":
     main()
